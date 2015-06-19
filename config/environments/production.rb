a4e3a8d222df9772b3213b4a4a086710d25b9c97
vijayaraj <vraj.mdu+git@gmail.com>
2015-06-19 20:27:23 +0530

    Adding prod mailer host
    Signed-off-by: vijayaraj <vraj.mdu+git@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,19 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => "http://ec2-54-175-100-128.compute-1.amazonaws.com" }
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => "587",
+    :authentication => :plain,
+    :user_name      => "issueuploader@gmail.com",
+    :password       => "issueuploader@1",
+    :domain         => "domain.com"
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
